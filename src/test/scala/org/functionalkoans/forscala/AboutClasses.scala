0d8350a9188fe408fc62442a4813124c58c9ff76
jasonnerothin <jasonnerothin@superiorbiomining.com>
2013-04-23 11:10:28 -0500

    Done with AboutClasses

--- src/test/scala/org/functionalkoans/forscala/AboutClasses.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutClasses.scala
@@ -1,30 +1,34 @@
 package org.functionalkoans.forscala
 
 import support.KoanSuite
+import org.scalatest.matchers.ShouldMatchers
 
-class AboutClasses extends KoanSuite {
-
+// DONE
+class AboutClasses extends KoanSuite with ShouldMatchers{
 
   // you can define class with var or val parameters
   class ClassWithVarParameter(var description: String)
 
   class ClassWithValParameter(val name: String)
 
+  // you can define class with private fields
+  class ClassWithPrivateFields(name: String)
+
   koan("val parameters in class definition define getter") {
-    val aClass = new ClassWithValParameter("name goes here")
-    aClass.name should be(__)
+    val str = "name goes here"
+    val aClass = new ClassWithValParameter(str)
+    aClass.name should be(str)
   }
 
   koan("var parameters in class definition define getter and setter") {
-    val aClass = new ClassWithVarParameter("description goes here")
-    aClass.description should be(__)
+    val str = "description goes here"
+    val aClass = new ClassWithVarParameter(str)
+    aClass.description should be(str)
 
-    aClass.description = "new description"
-    aClass.description should be(__)
+    val str2 = "new description"
+    aClass.description = str2
+    aClass.description should be(str2)
   }
-
-  // you can define class with private fields
-  class ClassWithPrivateFields(name: String)
 
   koan("fields defined internally are private to class") {
     val aClass = new ClassWithPrivateFields("name")
